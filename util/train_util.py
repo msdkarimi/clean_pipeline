338552157474706e8b1a491c7d31509236dd6d6e
massoud <itsmasoudkarimi@gmail.com>
2025-04-16 15:39:02 +0200

    init

--- util/train_util.py
+++ util/train_util.py
@@ -87,6 +87,7 @@ def take_step(model, sup_con_loss, cls_loss, batch_idx, epoch, steps_per_epoch, 
 
 def train_epoch(model, train_loader, opts_lr_schedulers, epoch, steps_per_epoch, logger):
     model.train()
+    model.backbone.freezer()
 
     loss_meter_sup_con = AverageMeter()
     loss_meter_cls = AverageMeter()
@@ -210,4 +211,41 @@ def validation(model, val_loader, epoch, logger):
 
     return f_score
 
+def from_pretrained(model, load_dir, logger):
+    state_dict = torch.load(load_dir)['model']
+    state_dict = align_and_update_state_dicts(model.state_dict(), state_dict, logger)
+    model.load_state_dict(state_dict, strict=False)
+    return model
 
+def align_and_update_state_dicts(model_state_dict, ckpt_state_dict, logger):
+    model_keys = sorted(model_state_dict.keys())
+    ckpt_keys = sorted(ckpt_state_dict.keys())
+    result_dicts = {}
+    matched_log = []
+    unmatched_log = []
+    unloaded_log = []
+    for model_key in model_keys:
+        model_weight = model_state_dict[model_key]
+        if model_key in ckpt_keys:
+            ckpt_weight = ckpt_state_dict[model_key]
+            if model_weight.shape == ckpt_weight.shape:
+                result_dicts[model_key] = ckpt_weight
+                ckpt_keys.pop(ckpt_keys.index(model_key))
+                matched_log.append("Loaded {}, Model Shape: {} <-> Ckpt Shape: {}".format(model_key, model_weight.shape,
+                                                                                          ckpt_weight.shape))
+            else:
+                unmatched_log.append(
+                    "*UNMATCHED* {}, Model Shape: {} <-> Ckpt Shape: {}".format(model_key, model_weight.shape,
+                                                                                ckpt_weight.shape))
+        else:
+            unloaded_log.append("*UNLOADED* {}, Model Shape: {}".format(model_key, model_weight.shape))
+
+    for info in matched_log:
+        logger.info(info)
+    for info in unloaded_log:
+        logger.warning(info)
+    for key in ckpt_keys:
+        logger.warning("$UNUSED$ {}, Ckpt Shape: {}".format(key, ckpt_state_dict[key].shape))
+    for info in unmatched_log:
+        logger.warning(info)
+    return result_dicts
